cb9f04a5b7d3ee7d77ab2ecf5b2e06cbeb09ad24
@paulopatto <paulopatto@gmail.com>
2013-09-03 09:52:51 -0300

    Innitial go file

--- src/frequencia.go
+++ src/frequencia.go
@@ -1,3 +1,17 @@
 package main
 import "fmt"
 
+/*
+  Escreva um programa que recebe como parâmetro uma string qualquer e retorna um dicionário/hashtable
+  no qual as chaves são todos os caracteres que ocorrem na string.
+  Não deve ser utilizada nenhuma função pronta da linguagem para a contagem de caracteres.
+*/
+
+func main(){
+  usage()
+}
+func usage(){
+  fmt.Printf("Usage command: frequencia --options $string\n")
+  fmt.Printf("Options: \n")
+  fmt.Printf("\t--ic \tIgnore case string\n")
+}
