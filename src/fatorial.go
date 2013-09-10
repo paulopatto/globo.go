a9f1de7075315d9b106f62fbb2fe7089e2850230
@paulopatto <paulopatto@gmail.com>
2013-09-09 21:22:45 -0300

    Fatorail OK

--- src/fatorial.go
+++ src/fatorial.go
@@ -17,12 +17,24 @@ func main(){
   }
   n, err := strconv.Atoi(os.Args[1])
   if err == nil{
-    fmt.Printf("Fatorial do número %q\n\t", os.Args[1])
+    fmt.Printf("Fatorial do número %q\n", os.Args[1])
+    fmt.Printf("recursivo\n\t")
     fmt.Println(factorial(n))
+    fmt.Printf("Agora com loop for while\n\t")
+    fmt.Println(factorial2(n))
   }
 }
 
 func factorial(number int) int {
   if number < 2 { return number }
   return number * factorial(number - 1)
+}
+
+func factorial2(number int) int{
+  fat := 1
+  for ; number > 1 ; {
+    fat *= number
+    number--
+  }
+  return fat
 }
